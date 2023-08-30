7b0de8d1b395065da8d5c584ea756113b0945a85
nigraum <yan.corintima@hotmail.com>
2023-08-30 00:14:24 -0300

    Add calc exercise

---
+++ calc.cs
@@ -0,0 +1,10 @@
+using System;
+
+class calc {
+  public static void Main(string[] args) {
+    int n1 = Int32.Parse(System.Console.ReadLine().Trim());
+    int n2 = Int32.Parse(System.Console.ReadLine().Trim());
+    int x = n1 + n2;
+    Console.WriteLine("X = {0}", X);
+  }
+}
